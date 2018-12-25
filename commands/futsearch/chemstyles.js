2e2455bbc47f969b3bc031a72aad5d0634167574
Tjird <tjeerd@tjird.nl>
2018-12-25 08:13:04 +0100

    Teamgeest en positiion changes added

--- commands/futsearch/chemstyles.js
+++ commands/futsearch/chemstyles.js
@@ -5,59 +5,51 @@ const tabletojson = require('tabletojson');
 class CheapestSearchCommand extends commando.Command {
     constructor(client) {
         super(client, {
-            name: 'cheapest',
+            name: 'cheapestchem',
             group: 'fut_cheapest',
-            memberName: 'cheapest',
+            memberName: 'cheapestchem',
             description: 'Zoekt de goedkoopste per rating'
         });
     }
 
     async run(message, args) {
         if (message.author.bot) return;
-        // var split = args.split(" ");
-        // var slice = split.slice(0, 1);
-        // var slice2 = split.slice(1, 2);
 
-        var url = `https://www.futbin.com/consumables/Position%20Change`;
+        var url = `https://www.futbin.com/consumables/Chemistry%20Styles`;
 
-        // if (slice2[0]) {
-        //     var slice2 = slice2[0].toString().toUpperCase();
-        // } else {
-        //     message.reply("Vul aub een console in als 2de argument.")
-        // }
         tabletojson.convertUrl(
             url,
             function (tablesAsJson) {
-                console.log(tablesAsJson);
-                // tableinjson(tablesAsJson[2]);
+                tableinjson(tablesAsJson);
             }
         );
 
-        // function tableinjson(table) {
-        //     const title = `${slice} overal rating spelers`;
-        //     const description = `Dit is een lijst met de 10 goedkoopste spelers\n van ${slice} overal rating voor het platform ${slice2}`;
+        function tableinjson(table) {
+            const title = `Prijzen teamgeest verbruiksitems`;
+            const description = `Dit is een lijst met alle 23 teamgeet verbruikitmes hun prijzen.`;
+            const author = `Alle prijzen van de teamgeet verbruiksitems`;
 
-        //     var markersprice = [];
-        //     var markersplayer = [];
-        //     for (var i = 0; i < 11; ++i) {
-        //         markersprice[i] = `${table[i].PS}`;
-        //         markersplayer[i] = `- ${table[i].Name} ${table[i].POS}/${table[i].VER} ${table[i].RAT}`;
-        //     }
+            var xboxprices = [];
+            var psprices = [];
+            for (var i = 0; i < 23; ++i) {
+                xboxprices[i] = `- ${table[1][i][0]} ${table[1][i][1]}`;
+                psprices[i] = `- ${table[0][i][0]} ${table[0][i][1]}`;
+            }
 
-        //     const field1 = `${markersplayer[0]}\n${markersplayer[1]}\n${markersplayer[2]}\n${markersplayer[3]}\n${markersplayer[4]}\n${markersplayer[5]}\n${markersplayer[6]}\n${markersplayer[7]}\n${markersplayer[8]}\n${markersplayer[9]}`;
-        //     const field2 = `${markersprice[0]}\n${markersprice[1]}\n${markersprice[2]}\n${markersprice[3]}\n${markersprice[4]}\n${markersprice[5]}\n${markersprice[6]}\n${markersprice[7]}\n${markersprice[8]}\n${markersprice[9]}`;
+            const field1 = `${psprices[0]}\n${psprices[1]}\n${psprices[2]}\n${psprices[3]}\n${psprices[4]}\n${psprices[5]}\n${psprices[6]}\n${psprices[7]}\n${psprices[8]}\n${psprices[9]}\n${psprices[10]}\n${psprices[11]}\n${psprices[12]}\n${psprices[13]}\n${psprices[14]}\n${psprices[15]}\n${psprices[16]}\n${psprices[17]}\n${psprices[18]}\n${psprices[19]}\n${psprices[20]}\n${psprices[21]}\n${psprices[22]}`;
+            const field2 = `${xboxprices[0]}\n${xboxprices[1]}\n${xboxprices[2]}\n${xboxprices[3]}\n${xboxprices[4]}\n${xboxprices[5]}\n${xboxprices[6]}\n${xboxprices[7]}\n${xboxprices[8]}\n${xboxprices[9]}\n${xboxprices[10]}\n${xboxprices[11]}\n${xboxprices[12]}\n${xboxprices[13]}\n${xboxprices[14]}\n${xboxprices[15]}\n${xboxprices[16]}\n${xboxprices[17]}\n${xboxprices[18]}\n${xboxprices[19]}\n${xboxprices[20]}\n${xboxprices[21]}\n${xboxprices[22]}`;
 
-        //     const embed = new Discord.RichEmbed()
-        //         .setColor(0x2FF37A)
-        //         .setAuthor(`Goedkoopste spelers per overal rating lijst`)
-        //         .setTitle(title)
-        //         .setURL(url)
-        //         .setDescription(description)
-        //         .setFooter("FUT Searcher v.1.0.0 | Prijzen van FUTBIN | Made by Tjird, inspired by ajpiano", "https://tjird.nl/fut1.jpg")
-        //         .addField("Naam", field1, true)
-        //         .addField("Prijs", field2, true)
-        //     message.reply("hier is jou aangevraagde lijst:", { embed });
-        // };
+            const embed = new Discord.RichEmbed()
+                .setColor(0x2FF37A)
+                .setTitle(title)
+                .setAuthor(author)
+                .setURL(url)
+                .setDescription(description)
+                .setFooter("FUT Searcher v.1.0.0 | Prijzen van FUTBIN | Made by Tjird, inspired by ajpiano", "https://tjird.nl/fut1.jpg")
+                .addField("PS", field1, true)
+                .addField("XBOX", field2, true)
+            message.reply("hier is jou aangevraagde lijst:", { embed });
+        };
 
     }
 }
---
+++ commands/futsearch/positionchange.js
@@ -0,0 +1,57 @@
+const commando = require('discord.js-commando');
+const Discord = require('discord.js');
+const tabletojson = require('tabletojson');
+
+class CheapestSearchCommand extends commando.Command {
+    constructor(client) {
+        super(client, {
+            name: 'cheapestpos',
+            group: 'fut_cheapest',
+            memberName: 'cheapestpos',
+            description: 'Geeft prijzelijst van alle positie wijzingen'
+        });
+    }
+
+    async run(message, args) {
+        if (message.author.bot) return;
+
+        var url = `https://www.futbin.com/consumables/Position%20Change`;
+
+        tabletojson.convertUrl(
+            url,
+            function (tablesAsJson) {
+                tableinjson(tablesAsJson);
+            }
+        );
+
+        function tableinjson(table) {
+            const title = `Prijzen postitie verbruiksitems`;
+            const description = `Dit is een lijst met alle 20 positie wijziging verbruikitmes hun prijzen.`;
+            const author = `Alle prijzen van de positie wijzigingen verbruiksitems`;
+
+            var xboxprices = [];
+            var psprices = [];
+            for (var i = 0; i < 20; ++i) {
+                xboxprices[i] = `- ${table[1][i][0]} ${table[1][i][1]}`;
+                psprices[i] = `- ${table[0][i][0]} ${table[0][i][1]}`;
+            }
+
+            const field1 = `${psprices[0]}\n${psprices[1]}\n${psprices[2]}\n${psprices[3]}\n${psprices[4]}\n${psprices[5]}\n${psprices[6]}\n${psprices[7]}\n${psprices[8]}\n${psprices[9]}\n${psprices[10]}\n${psprices[11]}\n${psprices[12]}\n${psprices[13]}\n${psprices[14]}\n${psprices[15]}\n${psprices[16]}\n${psprices[17]}\n${psprices[18]}\n${psprices[19]}`;
+            const field2 = `${xboxprices[0]}\n${xboxprices[1]}\n${xboxprices[2]}\n${xboxprices[3]}\n${xboxprices[4]}\n${xboxprices[5]}\n${xboxprices[6]}\n${xboxprices[7]}\n${xboxprices[8]}\n${xboxprices[9]}\n${xboxprices[10]}\n${xboxprices[11]}\n${xboxprices[12]}\n${xboxprices[13]}\n${xboxprices[14]}\n${xboxprices[15]}\n${xboxprices[16]}\n${xboxprices[17]}\n${xboxprices[18]}\n${xboxprices[19]}`;
+
+            const embed = new Discord.RichEmbed()
+                .setColor(0x2FF37A)
+                .setTitle(title)
+                .setAuthor(author)
+                .setURL(url)
+                .setDescription(description)
+                .setFooter("FUT Searcher v.1.0.0 | Prijzen van FUTBIN | Made by Tjird, inspired by ajpiano", "https://tjird.nl/fut1.jpg")
+                .addField("PS", field1, true)
+                .addField("XBOX", field2, true)
+            message.reply("hier is jou aangevraagde lijst:", { embed });
+        };
+
+    }
+}
+
+module.exports = CheapestSearchCommand;
