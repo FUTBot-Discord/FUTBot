a78d51f6ab3027cb2c8a778150bdae6c8cd59746
Tjird <tjeerd@tjird.nl>
2019-08-08 05:00:30 +0200

    proxies <3

--- commands/player.js
+++ commands/player.js
@@ -1,4 +1,11 @@
+const rp = require("request-promise");
+
 exports.run = async (client, message, args) => {
-
-
+    var reqOpts = {
+        url: "http://mcstatus.tjird.eu/check/play.dusdavidgames.nl",
+        method: "GET",
+        headers: { "Cache-Control": "no-cache" },
+        proxy: "http://185.216.162.75:3128"
+    };
+    const request = await rp(reqOpts);
 }
