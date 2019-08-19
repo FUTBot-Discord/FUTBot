45c9e4b3c4a608370f7b2e853b0d2b26c9e1c005
Tjird <tjeerd@tjird.nl>
2019-08-20 01:10:38 +0200

    changes

--- events/ready.js
+++ events/ready.js
@@ -1,3 +1,6 @@
+const DBL = require("dblapi.js");
+const dbl = new DBL('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjUyMDY5NDYxMjA4MDMyODcwOSIsImJvdCI6dHJ1ZSwiaWF0IjoxNTQ3NjMyOTU2fQ.Q8mbinEz3TtHhK3rU5WVVou3qyiirBohq9WR2MsPWJc', client);
+
 module.exports = (client) => {
     let usercount = 0;
     
@@ -8,7 +11,14 @@ module.exports = (client) => {
     console.log(`Logged in as ${client.user.tag} and looking at ${usercount} users(${client.guilds.size} guilds). `
         + `Shard ID: ${(client.shard.id + 1)}/${client.shard.count}.`);
 
-    client.user.setActivity(`${usercount} users`, { type: 'WATCHING' })
+    client.user.setActivity(`${client.guilds.size} servers`, { type: 'WATCHING' });
+    dbl.postStats(client.guilds.size);
+
+    setInterval(() => {
+        dbl.postStats(client.guilds.size);
+        client.user.setActivity(`${client.guilds.size} servers`, { type: 'WATCHING' });
+    }, 1800000)
+
 
     console.log("====================")
 
