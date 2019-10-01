dd7963499b993595717aee61d9e0e53185261477
Tjird <tjeerd@tjird.nl>
2019-10-01 21:36:34 +0200

    new commands

---
+++ commands/donate.js
@@ -0,0 +1,3 @@
+exports.run = async (client, message, args) => {
+    return message.reply('you can help me maintain the bot by donating with the following link: http://futbot.tjird.eu/donate/');
+}
