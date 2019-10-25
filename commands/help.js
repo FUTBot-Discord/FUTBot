c51256804591df20d5e8cb05e945e4494faeed49
Tjird <tjeerd@tjird.nl>
2019-10-25 12:13:12 +0200

    regdt

--- commands/help.js
+++ commands/help.js
@@ -1,3 +1,3 @@
 exports.run = async (client, message, args) => {
-    return message.reply("all the commands with there documentation are available on this site. \nhttps://futbot.tjird.eu\nDo you have a question that is not answered after reading the documentation? Join the supported Discord! https://discord.gg/KUnh4fc");
+    return message.reply("all the commands with there documentation are available on this site. \nhttps://futbot.tjird.eu\nDo you have a question that is not answered after reading the documentation? Join the supported Discord! https://discord.gg/KUnh4fc\n\nDo you want to get the most out of Discord for Fifa? Use also the \"Profit Bot\" https://www.profitbot.tech/");
 }
