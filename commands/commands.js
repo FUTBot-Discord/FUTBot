698c2c14eaf30580d9624839b431d1be6ac2d146
Tjird <tjeerd@tjird.nl>
2019-11-06 19:12:11 +0100

    commands command

---
+++ commands/commands.js
@@ -0,0 +1,32 @@
+const { RichEmbed } = require('discord.js');
+
+exports.run = async (client, message, args) => {
+    const embed = new RichEmbed()
+        .setTitle("Commands page")
+        .setDescription("Any question about an ommand? Join the [Supported Discord](https://discord.gg/KUnh4fc) or take a look at the [documentation](https://futbot.tech/documentation).\n" +
+            "If an argument has <> around it will mean that it's **required**. If an argument has \[\] around it will mean that it's **not required**.\n" +
+            "Command // Example")
+        .addField('Player prices', "player <playername> \[rating\] // fut!player van dijk 90" +
+            "playerex <playername> \[rating\] // fut!playerex mbappe\n" +
+            "cheapest <rating> <console> // fut!cheapest 83-84 ps\n" +
+            "top <console> \[type\] // fut!top xbox otw\n" +
+            "totw **NO ARGS** // fut!totw", false)
+        .addField('Consumables', "chemistry **NO ARGS** // fut!chemistry\n" +
+            "position **NO ARGS** // fut!position", false)
+        .addField("SBC", "sbc list **NO ARGS** // fut!sbc list\n" +
+            "sbc get <sbc_id> // fut!sbc get 8", false)
+        .addField('Simple calculator', "tax be <price> \[amount\] // fut!tax be 80000 4\n" +
+            "tax profit <buy-price> <sell-price> \[amount\] // fut!tax profit 1600 2000 9\n" +
+            "tax tax <price> \[amount\] // fut!tax tax 4500 2", false)
+        .addField("Util", "donate **NO ARGS** // fut!donate\n" +
+            "help **NO ARGS** // fut!help\n" +
+            "invite **NO ARGS** // fut!invite\n" +
+            "ping **NO ARGS** // fut!ping\n" +
+            "prefix change <prefix> // fut!prefix change fut?", false)
+        .addBlankField(false)
+        .addField("Console list", "- ps\n- xbox\n- pc", false)
+        .addField("Type list", "- gold OR golds\n- icon OR icons\n- otw OR otws", false)
+        .setFooter("FUTBot v.2.0.0 | Made by Tjird#0001", "https://tjird.nl/futbot.jpg");
+
+    return message.channel.send(embed);
+}
--- commands/help.js
+++ commands/help.js
@@ -1,3 +1,13 @@
+const { RichEmbed } = require('discord.js');
+
 exports.run = async (client, message, args) => {
-    return message.reply("all the commands with there documentation are available on this site. \nhttps://futbot.tjird.eu\nDo you have a question that is not answered after reading the documentation? Join the supported Discord! https://discord.gg/KUnh4fc\n\nDo you want to get the most out of Discord for Fifa? Use also the \"Profit Bot\" https://www.profitbot.tech/");
+    const embed = new RichEmbed()
+        .setTitle('Help page')
+        .setDescription('We here at the FUTBot really want to help you.\n' +
+            'Do you want to know all the commands? Use the ** commands ** (fut!commands) command to see all available commands.\n' +
+            "Can't you understand what to do or what else? Join the supported Discord server! [Click here](https://discord.gg/KUnh4fc) to join it.\n" +
+            "All the information could also be found at the documentation webpage(https://futbot.tech/documentation). If you [click here](https://futbot.tech/documentation) you will be redirected to the webpage.\n\n" +
+            "Want to get some advanced profit calculation? Take a look at the \"Profit\" bot also. It can be found [here](https://www.profitbot.tech/).");
+
+    return message.channel.send(embed);
 }
