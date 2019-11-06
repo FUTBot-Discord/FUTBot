a5b67da111b1ccdce8d3d6943986ffdf5b3118cd
Tjird <tjeerd@tjird.nl>
2019-11-06 19:49:04 +0100

    ghfsd

--- commands/support.js
+++ commands/support.js
@@ -1,3 +1,5 @@
 exports.run = async (client, message, args) => {
-    return message.reply("use the `commands` command to see all commands. Do you have a question that is not answered after seeing the commands? Join the supported Discord! https://discord.gg/KUnh4fc\n\nDo you want to get the most out of Discord for Fifa? Use also the \"Profit Bot\" https://www.profitbot.tech/");
+    const cmd = client.commands.get("help");
+
+    return cmd.run(client, message, args);
 }
