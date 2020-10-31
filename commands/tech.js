2b19f4585fff8e9e6766c80e181334dd2099e48f
Tjird <tjeerd@tjird.nl>
2020-10-31 16:31:18 +0100

    change tech commandooooo

--- commands/tech.js
+++ commands/tech.js
@@ -1,5 +1,5 @@
 exports.run = async (client, message, args) => {
     if (!args[0]) return message.reply("You need to fill in a number.");
 
-    return message.reply(parseFloat(args[0]) * 1.15);
+    return message.reply(parseFloat(args[0]) * 1.15 + " // " + args[0]);
 }
