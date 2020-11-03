85033e2541f70d60e84f1671412750913ea80b6e
Tjird <tjeerd@tjird.nl>
2020-11-03 15:12:37 +0100

    ratio add to tech command

--- commands/tech.js
+++ commands/tech.js
@@ -1,5 +1,5 @@
 exports.run = async (client, message, args) => {
     if (!args[0]) return message.reply("You need to fill in a number.");
-
-    return message.reply(parseFloat(args[0]) * 1.15 + " // " + args[0]);
+    if (args[1]) return message.reply(parseFloat(args[0]) * parseFloat(args[1]) + " // " + args[0]); 
+    else return message.reply(parseFloat(args[0]) * 1.15 + " // " + args[0]);
 }
